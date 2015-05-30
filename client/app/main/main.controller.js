facad77cd562c6588f6e2f160a9e4f98d488f5bc
rodrigo <reyes.rr@gmail.com>
2015-05-30 12:04:41 +0200

    refactored newsletter subscription as a component

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,39 +6,4 @@ angular.module('hackathonCitoyenApp')
 
     $scope.message = "Yop";
 
-    $scope.emailOptions = {
-      types: [
-        {id:"association", label: "je représente une association", choiceLabel: "Association"},
-        {id:"dev", label: "Je suis développeur", choiceLabel: "Développeur"},
-        {id:"designer", label: "Je suis designer, graphiste", choiceLabel: "Designer, graphiste"},
-        {id:"autre", label: "Je suis intéressé", choiceLabel: "Autre"}
-      ]
-    };
-    $scope.emailData = {
-      address: "",
-      type: {id: "none", label: "Je suis..."},
-      registered: false
-    };
-
-    $scope.subscribeNewsletter = function() {
-      if ($scope.emailData.address.length) {
-        $scope.emailData.processing = true;
-        $scope.emailData.error = undefined;
-        $http.post('/api/subscribers', {
-          email: $scope.emailData.address,
-          type: {
-            id: $scope.emailData.type.id,
-            label: $scope.emailData.type.choiceLabel
-          }
-        }).success(function(data,status) {
-          $scope.emailRegistered = true;
-          $scope.emailData.processing = false;
-        }).error(function(data,status){
-          $("email-sub-error").modal('show');
-          $scope.emailData.processing = false;
-          });
-      } else {
-        $scope.emailData.error = "L'email que vous avez saisi n'est pas valide."
-      }
-    };
 });
