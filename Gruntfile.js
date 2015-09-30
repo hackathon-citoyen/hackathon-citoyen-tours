9646ca91490b21db48a9ba9bcedac33555be602d
rodrigo <reyes.rr@gmail.com>
2015-09-30 08:08:09 -0700

    grunt should copy assets/*.pdf too

--- Gruntfile.js
+++ Gruntfile.js
@@ -355,6 +355,7 @@ module.exports = function (grunt) {
             '.htaccess',
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
+            'assets/*.pdf',
             'assets/fonts/**/*',
             'index.html'
           ]
